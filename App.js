9e420d7e1b0063f68ffd904a7c1eab0fb6204127
Sunainasingh09 <ssunaina700@gmail.com>
2023-06-12 13:40:55 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import {View,Text} from "react-native";
+
+class App extends Component{
+    render(){
+        return(
+        <View>
+            <Text>React Native</Text>
+        </View>
+        )
+    }
+    }
+
+export default App;
